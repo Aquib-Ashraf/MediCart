3175313a1b6fb48efa575312c5b3c73b4de86512
Aquib-Ashraf <aquibashraf000786@gmail.com>
2025-06-15 00:51:42 +0530

    login and register api created

--- backend/backend.js
+++ backend/backend.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
+import userRouter from './routes/userRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -18,6 +19,8 @@ app.use(cors({origin: allowedOrigins , credentials : true}));
 
 
 app.get('/',(req,res)=> res.send("API is working"));
+
+app.use('/api/user',userRouter);
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`)
