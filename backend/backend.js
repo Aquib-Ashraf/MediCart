5d40f79d14656fa4e3272dbb3e4f9884e2c79ce4
Aquib-Ashraf <aquibashraf000786@gmail.com>
2025-06-15 14:24:12 +0530

    completed product api

--- backend/backend.js
+++ backend/backend.js
@@ -5,11 +5,15 @@ import 'dotenv/config';
 import connectDB from './configs/db.js';
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
+import productRouter from './routes/productRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 await connectDB();
+await connectCloudinary();
+
 // Allowed multiple origins
 const allowedOrigins = ['http://localhost:5173']
 
@@ -22,7 +26,8 @@ app.use(cors({origin: allowedOrigins , credentials : true}));
 app.get('/',(req,res)=> res.send("API is working"));
 
 app.use('/api/user',userRouter);
-app.use('/api/seller',sellerRouter)
+app.use('/api/seller',sellerRouter);
+app.use('/api/product',productRouter);
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`)
